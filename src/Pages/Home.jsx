7d6961ac5e1ddf73d1ea6a856a6668ea6e5a0112
pradheepa jaya shree <pradheepajayashree@gmail.com>
2025-12-19 14:33:11 +0530

    theme toggle done-i.e.,dark or light

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,9 @@
+import { ThemeToggle } from "../components/ThemeToggle";
 
 export const Home = () => {
     return <div className="min-h-screen ">
         {/*theme toggle*/}
+        <ThemeToggle />
 
         {/*background effects*/}
 
