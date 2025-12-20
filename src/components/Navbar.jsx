0fffc1b11eeb737cd4dcb2f07294ed700992b871
pradheepa jaya shree <pradheepajayashree@gmail.com>
2025-12-20 15:13:39 +0530

    hovering over menu done for both desktop and mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 
 import { useEffect, useState} from "react";
 import { cn } from "../lib/utils";
+import { Menu, X } from "lucide-react";
+
 
 const navItems = [
     {name: "Home",href:"#hero"},
@@ -13,6 +15,7 @@ const navItems = [
 
 export const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const[isMenuOpen, setIsMenuOpen] = useState(true);
 
     useEffect(()=>{
         const handleScroll = () => {
@@ -24,15 +27,50 @@ export const Navbar = () => {
     }, [])
     return (
     <nav className={cn("fixed w-full z-40 transition-all duration-300", isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-x5":"py-5")} >
-        <div className="container flex items-center justify-between">
+        <div className="container flex items-center justify-between pr-20">
             <a className="text-xl font-bold text-primary flex items-center">
-                <span className="relative z-10">
-                    <span className="text-glow text-foreground">
+                <span className="relative z-10 text-indigo-400">
+                    <span className="text-foreground glow text-indigo-400">
                         Pradheepa Jayashree's  
                     </span>{" "}
                      Portfolio
                 </span>
             </a>
+
+            {/*desktop*/}
+            <div className="hidden md:flex space-x-8">
+                {navItems.map((item,key) =>(
+                    <a key={key} href={item.href} className="text-foreground/70 hover:text-indigo-600 hover:text-glow hover:scale-105 transition-all duration-300">
+                        {item.name}
+                    </a>
+                )) }
+            </div>
+
+            {/*mobile*/}
+
+            <button onClick={()=>setIsMenuOpen((prev) => !prev)}
+             className="md:hidden p-2 text-foreground z-50"
+             arial-label={isMenuOpen ? "close Menu" : "Open Menu"}
+             >
+                {" "}
+                 {isMenuOpen? <X size={24}/> : <Menu size={24}/>} {" "}</button>
+
+
+            <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex-col items-center justify-center transition-all duration-300 md:hidden",
+             isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+        )}>
+            <div className="flex flex-col space-y-8 text-xl">
+                {navItems.map((item,key) =>(
+                    <a key={key} href={item.href} className="text-foreground/80 hover:text-indigo-600 hover:text-glow hover:scale-105 transition-all duration-300"
+                       onClick={() => setIsMenuOpen(false)}
+                    >
+                        
+                        {item.name}
+                    </a>
+                )) }
+            </div>
+            </div>
+            
 
         </div>
 
@@ -40,4 +78,4 @@ export const Navbar = () => {
 
     </nav>
 );
-}
+};
