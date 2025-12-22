8309eb53e9b53d0a80b8702c09d5b44c36973811
pradheepa jaya shree <pradheepajayashree@gmail.com>
2025-12-22 21:34:08 +0530

    hero section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Navbar } from "../components/Navbar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import {StarBackground} from "@/components/StarBackground";
-
+import { HeroSection } from "../components/HeroSection";
 export const Home = () => {
     return <div className="min-h-screen ">
         {/*theme toggle*/}
@@ -14,6 +14,9 @@ export const Home = () => {
         <Navbar />
 
         {/* main content*/}
+        <main>
+            <HeroSection />
+        </main>
 
         {/* footer */}
     </div>;
